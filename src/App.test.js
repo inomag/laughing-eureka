a420953e80f90fea2cccb597180cf676689ee06d
Inomag <pratiksr12345@gmail.com>
2021-08-25 11:40:48 +0530

    Added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,3 @@ test("renders learn react link", () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
 });
-
-test("renders learn react link", () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-});
